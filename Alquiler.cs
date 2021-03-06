070ab29e4cb2ef09ee71ad7d693ed6bad5584f1e
Miguel Santos <fersan21049@gmail.com>
2021-03-06 17:06:41 -0600

    creacion clase alquiler para el control de fechas y kilometros

---
+++ Alquiler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Alquiler
+    {
+        string fecha_al;
+        string fecha_de;
+        double kilrecorridos;
+
+        public string Fecha_al { get => fecha_al; set => fecha_al = value; }
+        public string Fecha_de { get => fecha_de; set => fecha_de = value; }
+        public double Kilrecorridos { get => kilrecorridos; set => kilrecorridos = value; }
+    }
+}
