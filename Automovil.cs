52f23e24a80ca8a8276175aabc0bd6bd61f15751
Miguel Santos <fersan21049@gmail.com>
2021-03-06 16:42:54 -0600

    Creacion de la clase vehiculos y de la clase clientes para el control de los mismos

---
+++ Automovil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Automovil
+    {
+        string placa;
+        string marca;
+        string modelo;
+        string color;
+        double preciok;
+
+        public string Placa { get => placa; set => placa = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+        public string Color { get => color; set => color = value; }
+        public double Preciok { get => preciok; set => preciok = value; }
+    }
+}
